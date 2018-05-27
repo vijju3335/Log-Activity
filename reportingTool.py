cef116d0f90c4d22624ef2b1eeeefc7747b431e8
Vijju3335 <34649168+vijju3335@users.noreply.github.com>
2018-05-27 21:53:15 +0530

    pep8 used to validate

--- reportingTool.py
+++ reportingTool.py
@@ -2,32 +2,42 @@
 import psycopg2
 
 # Below lines helps us Connect to the PostgreSQL DB if not, rasies exception
-try:
-    connect = psycopg2.connect(
+connector = psycopg2.connect(
         dbname='news',
         user='vagrant',
         password='vagrant',
         host='localhost',
         port='5432'
         )
-    cur = connect.cursor()
-except Exception as e:
-    print(e + "Error in connecting to database")
+cur = connector.cursor()
 
 
-def report_most_popular_articles():
-    # we get the most popular three articles of all time
-    ''' CREATE VIEW log_slug as SELECT replace(path,'/article/','') as slug, count(*) as views
-     FROM log
-     WHERE path <> '/' AND status ='200 OK' GROUP BY path;'''
-    psql = ''' select title, views from log_slug INNER JOIN articles on
-    articles.slug = log_slug.slug order by views desc limit 3; '''
+def report_log_error_percentage():
+    # Prints Log errors percentage more than 1%
+    '''
+    CREATE VIEW log_fail as SELECT Date(time), count(Date(time))
+    FROM log
+    WHERE status='404 NOT FOUND' GROUP BY Date(time) ORDER BY Date(time);
+
+
+    '''
+    psql = '''
+    SELECT log_fail.date ,(log_fail.count*100.00 / log_total.count) AS
+    percentage FROM log_fail INNER JOIN log_total
+    ON log_fail.date = log_total.date
+    AND (log_fail.count*100.00 / log_total.count) >1
+    ORDER BY (log_fail.count*100.00 / log_total.count) desc;
+    '''
     cur.execute(psql)
-    res = cur.fetchall()
+    reported = cur.fetchall()
     # Prints report
-    print(" \n What are the most popular three articles of all time ? \n")
-    for report in res:
-        print(' "{0}"   ===>   {1} views'.format(report[0], report[1]))
+    print(" \n  3. Days on which more than 1% of requests lead to errors ? ")
+    for report in reported:
+        print(
+            '\n  On ' + str(report[0]) +
+            '   ===>   ' + '%.1f' % report[1] +
+            '% errors\n'
+            )
 
 
 def report_most_popular_authors():
@@ -44,48 +54,38 @@ def report_most_popular_authors():
     WHERE path<>'/' AND status ='200 OK' GROUP BY path;
     '''
     psql = '''
-    select authors_name.name as author,
-    sum(log_slug.views) as views from log_slug join authors_name on
-    authors_name.slug=log_slug.slug
-    group by authors_name.name order by views desc;
+    SELECT authors_name.name AS author,
+    sum(log_slug.views) AS views FROM log_slug INNER JOIN authors_name
+    ON authors_name.slug=log_slug.slug
+    GROUP BY authors_name.name ORDER BY views desc;
     '''
     cur.execute(psql)
     reported = cur.fetchall()
     # Prints report
-    print("\n Who are the most popular article authors of all time ? \n")
+    print("\n  2. Who are the most popular article authors of all time ? \n")
     for report in reported:
-        print(' "{0}"   ===>   {1} views'.format(report[0], report[1]))
+        print('  "{0}"   ===>   {1} views'.format(report[0], report[1]))
 
 
-def report_log_error_percentage():
-    # Prints Log errors percentage more than 1%
+def report_most_popular_articles():
+    # we get the most popular three articles of all time
     '''
-    CREATE VIEW log_fail as SELECT Date(time), count(Date(time))
-    FROM log
-    WHERE status='404 NOT FOUND' GROUP BY Date(time) ORDER BY Date(time);
-
-    
+    CREATE VIEW log_slug as SELECT replace(path,'/article/','') AS slug,
+    count(*) AS views FROM log
+    WHERE path <> '/' AND status ='200 OK' GROUP BY path;
     '''
-    psql = '''
-    select log_fail.date ,(log_fail.count*100.00 / log_total.count) as
-    percentage from log_fail inner join log_total on
-    log_fail.date = log_total.date
-    where (log_fail.count*100.00 / log_total.count) >1
-    order by (log_fail.count*100.00 / log_total.count) desc;
-    '''
+    psql = ''' SELECT title, views FROM log_slug INNER JOIN articles ON
+    articles.slug = log_slug.slug ORDER BY views desc LIMIT 3; '''
     cur.execute(psql)
-    reported = cur.fetchall()
+    res = cur.fetchall()
     # Prints report
-    print(" \n Days on which more than 1% of requests lead to errors ? ")
-    for report in reported:
-        print(
-            '\n ' + str(report[0]) +
-            '   ===>   ' + '%.1f' % report[1] +
-            '% errors\n'
-            )
+    print(" \n  1. What are the most popular three articles of all time ? \n")
+    for report in res:
+        print('  "{0}"   ===>   {1} views'.format(report[0], report[1]))
+
 
 report_most_popular_articles()
 report_most_popular_authors()
 report_log_error_percentage()
 cur.close()
-connect.close()
+connector.close()
